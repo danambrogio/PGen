c270b60a833ce2fcfa26a1c5bf92f11819780281
Dan Ambrogio <dan.ambrogio@gmail.com>
2012-02-01 22:10:54 -0500

    Updated to version 1.1 Can now choose a 9 or 12 digit password

--- pgen.py
+++ pgen.py
@@ -1,4 +1,4 @@
-# Password Generator Program v1.0
+# Password Generator Program v1.1
 # pgen
 # Written by Dan "Anansi" Ambrogio
 #
@@ -10,6 +10,9 @@
 #   It also eliminates shared passwords between sites, so someone who
 #   finds out your Reddit password can't simply use it to access your
 #   email.
+#
+#   Both 9- and 12-digit passwords can be created, at the user's 
+#   discretion, to account for different websites' eccentricities.
 #
 #   One weakness this method has is that all passwords are hex values,
 #   meaning that they consist of characters from 0-9 and a-f, A-F. Thus,
@@ -26,23 +29,27 @@ import re
 def check_pword(password):
     '''
     This function checks the generated password to make sure it complies
-    to password standards. The generated password should:
-    - be [ADD LATER] characters long
-    - have lowercase, uppercase, and numbers
-    -
+    to password standards. The generated password should have lowercase,
+    uppercase, and numbers
+    
     The 'password' variable is a string containing the password to be
     checked. The function returns True if it is a valid password.
     '''
+    
+    # Makes a string containing all the digits and upper/lower characters
     valid = (string.digits + string.ascii_lowercase +
              string.ascii_uppercase)
-             
-    if len(password) == 9:
-        if any(i in valid for i in password):
-            return True
+
+
+    # Checks that each character is a number or letter
+    if any(i in valid for i in password):
+        if (sum(b in string.digits for b in password)) > 0:
+            if (sum(c in string.ascii_lowercase for c in password)) > 0:
+                return True
     else: return False
     
 
-def gen_pword(pass1, pass2, sitename, trial = 0):
+def gen_pword(pass1, pass2, sitename, digits = 9, trial = 0):
     '''
     This function generates a password based on the two user passwords
     and the site.
@@ -51,7 +58,13 @@ def gen_pword(pass1, pass2, sitename, trial = 0):
     The 'pass2' variable is a string containing the second user password.
     The 'sitename' variable is a string containing the website to generate
     the password for.
+    The 'digits' variable is an integer containing the number of digits
+    the password should have, 9 or 12
+    The 'trial' variable is an integer used when the generated password
+    does not meet standards; it increments the position in the hash to use
+    to try again for a valid password.
     '''
+    
     # Convert the passwords to SHA224 hashes and get the hex values
     pass1 = hashlib.sha224(bytearray(pass1, 'utf-8')).hexdigest()
     pass2 = hashlib.sha224(bytearray(pass2, 'utf-8')).hexdigest()
@@ -59,7 +72,7 @@ def gen_pword(pass1, pass2, sitename, trial = 0):
     
     # Take pseudorandom values from the hashes and combine them
     out = ""
-    for i in range(3):
+    for i in range((digits//3)):
         out += pass1[1+i+trial]       # Picked at random
         out += pass2[30-i-trial]      # Same
         out += sitename[1+i+trial]    # Same
@@ -78,13 +91,13 @@ if __name__ == "__main__":
     # Program starts here
     
     # User-selected input
-    p1, p2, site = '', '', ''
+    p1, p2, site, length = '', '', '', ''
     
     # Program output
     output = ''
     
     # Print instructions for the user
-    print( '\n\nPassword Generator Program v1.0' )
+    print( '\n\nPassword Generator Program v1.1' )
     print( 'Written by Dan "Anansi" Ambrogio')
     print( '________________________________\n' )
     print( 'This program creates a password for a website based on two' )
@@ -95,6 +108,18 @@ if __name__ == "__main__":
     print( "you're trying to access. Please note that punctuation" )
     print( "matters, so 'http://gmail.com' is not the same as 'gmail'\n")
     
+    # User chooses 9 or 12 digit password
+    while length == "":
+        length = input( "First, would you like a 9- or 12-digit password?")
+        
+        try:
+            if int(length) != 9 and int(length) != 12:
+                length = ''
+        except: 
+            print ( "Please choose 9 or 12." )
+            length = ''
+    length = int(length)
+           
     # User enters password 1
     while p1 == "":
         p1 = input( 'Enter your first password or q to quit: ' )
@@ -112,7 +137,7 @@ if __name__ == "__main__":
     # is marked as valid
     valid = False
     while valid == False:
-        output = gen_pword(p1, p2, site)
+        output = gen_pword(p1, p2, site, length)
         if (check_pword(output) == True): valid = True
     
     print( 'Your new password is: {}'.format(output) )
